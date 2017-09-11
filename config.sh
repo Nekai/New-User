c6cdae4e8d2c3414094e4b6e5d04c5537f5ad192
Nekai <sharpjacob657@gmail.com>
2017-09-11 19:54:12 -0400

    Fix

--- config.sh
+++ config.sh
@@ -9,7 +9,7 @@ echo ''
 
 sudo rm -rf ~/.ssh/authorized_keys && sudo rm -rf ~/.ssh/id_rsa
 sudo cp /home/cyclecloud/git/cyclecloud.pem ~/.ssh/id_rsa
-sudo cp /home/cgundlach/.ssh/authorized_keys ~/.ssh/authorized_keys
+sudo cp /home/cyclecloud/git/authorized_keys ~/.ssh/authorized_keys
 
 sudo chown $USER ~/.ssh/authorized_keys && sudo chown $USER ~/.ssh/id_rsa 
 
