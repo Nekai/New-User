cf785d1ead0880b949e12018471fe1c431aeafe3
Nekai <sharpjacob657@gmail.com>
2017-09-12 08:53:00 -0400

    fixed for new repository name

--- config.sh
+++ config.sh
@@ -8,8 +8,8 @@ echo | ssh-keygen -t rsa -P ''
 echo ''
 
 sudo rm -rf ~/.ssh/authorized_keys && sudo rm -rf ~/.ssh/id_rsa
-sudo cp /home/cyclecloud/git/cyclecloud.pem ~/.ssh/id_rsa
-sudo cp /home/cyclecloud/git/authorized_keys ~/.ssh/authorized_keys
+sudo cp /home/cyclecloud/new-user/cyclecloud.pem ~/.ssh/id_rsa
+sudo cp /home/cyclecloud/new-user/authorized_keys ~/.ssh/authorized_keys
 
 sudo chown $USER ~/.ssh/authorized_keys && sudo chown $USER ~/.ssh/id_rsa 
 
